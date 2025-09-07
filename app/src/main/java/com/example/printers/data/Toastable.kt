62b5c60633edb1f9f0b9ce61f8e5ff0c9b0cf251
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-09-07 14:33:12 +0300

    final project

--- app/src/main/java/com/example/printers/data/Toastable.kt
+++
@@ -1,7 +0,0 @@
-package com.example.printers.data
-
-import android.content.Context
-
-interface Toastable {
-    abstract fun makeToast(context: Context , name: String, ip: String)
-}
---
+++ app/src/main/java/com/example/printers/data/printers/DataManager.kt
@@ -0,0 +1,166 @@
+package com.example.printers.data.printers
+
+class DataManager {
+    val printers = listOf(
+        DataModel(
+            "Server A",
+            "Main Printer",
+            192168001,
+            isMain = true,
+            isOffline = false,
+            isError = false
+        ),
+        DataModel(
+            "Server B",
+            "Offline Printer",
+            192168002,
+            isMain = false,
+            isOffline = true,
+            isError = false
+        ),
+        DataModel(
+            "Server C",
+            "Error Printer",
+            192168003,
+            isMain = false,
+            isOffline = false,
+            isError = true
+        ),
+        DataModel(
+            "Server D",
+            "Main Printer",
+            192168004,
+            isMain = true,
+            isOffline = false,
+            isError = false
+        ),
+        DataModel(
+            "Server E",
+            "Offline Printer",
+            192168005,
+            isMain = false,
+            isOffline = true,
+            isError = false
+        ),
+        DataModel(
+            "Server F",
+            "Error Printer",
+            192168006,
+            isMain = false,
+            isOffline = false,
+            isError = true
+        ),
+        DataModel(
+            "Server G",
+            "Main Printer",
+            192168007,
+            isMain = true,
+            isOffline = false,
+            isError = false
+        ),
+        DataModel(
+            "Server H",
+            "Offline Printer",
+            192168008,
+            isMain = false,
+            isOffline = true,
+            isError = false
+        ),
+        DataModel(
+            "Server I",
+            "Error Printer",
+            192168009,
+            isMain = false,
+            isOffline = false,
+            isError = true
+        ),
+        DataModel(
+            "Server J",
+            "Main Printer",
+            192168010,
+            isMain = true,
+            isOffline = false,
+            isError = false
+        ),
+        DataModel(
+            "Server K",
+            "Offline Printer",
+            192168011,
+            isMain = false,
+            isOffline = true,
+            isError = false
+        ),
+        DataModel(
+            "Server L",
+            "Error Printer",
+            192168012,
+            isMain = false,
+            isOffline = false,
+            isError = true
+        ),
+        DataModel(
+            "Server M",
+            "Main Printer",
+            192168013,
+            isMain = true,
+            isOffline = false,
+            isError = false
+        ),
+        DataModel(
+            "Server N",
+            "Offline Printer",
+            192168014,
+            isMain = false,
+            isOffline = true,
+            isError = false
+        ),
+        DataModel(
+            "Server O",
+            "Error Printer",
+            192168015,
+            isMain = false,
+            isOffline = false,
+            isError = true
+        ),
+        DataModel(
+            "Server P",
+            "Main Printer",
+            192168016,
+            isMain = true,
+            isOffline = false,
+            isError = false
+        ),
+        DataModel(
+            "Server Q",
+            "Offline Printer",
+            192168017,
+            isMain = false,
+            isOffline = true,
+            isError = false
+        ),
+        DataModel(
+            "Server R",
+            "Error Printer",
+            192168018,
+            isMain = false,
+            isOffline = false,
+            isError = true
+        ),
+        DataModel(
+            "Server S",
+            "Main Printer",
+            192168019,
+            isMain = true,
+            isOffline = false,
+            isError = false
+        ),
+        DataModel(
+            "Server T",
+            "Offline Printer",
+            192168020,
+            isMain = false,
+            isOffline = true,
+            isError = false
+        )
+    )
+}
