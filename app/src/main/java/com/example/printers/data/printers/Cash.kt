6262ce2e054b02edc6b623a9e891dfc8a2a54c90
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-09-07 16:00:46 +0300

    payment classes

---
+++ app/src/main/java/com/example/printers/data/printers/Cash.kt
@@ -0,0 +1,6 @@
+package com.example.printers.data.printers
+
+class Cash: Payment {
+    override val name: String
+        get() = "Cash"
+}
---
+++ app/src/main/java/com/example/printers/data/printers/PayPal.kt
@@ -0,0 +1,7 @@
+package com.example.printers.data.printers
+
+class PayPal() : Payment  {
+    override val name: String
+        get() = "Pay_Pal"
+
+}
